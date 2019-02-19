030311dc9791f0323bf0303d69ed3d16f9550851
Roman Yanush <roman.yanush7@gmail.com>
2019-02-19 12:33:53 +0200

    added fixes for December & wrong class date enter

--- projects/ultipicker/src/lib/picker-block/picker-block.component.ts
+++ projects/ultipicker/src/lib/picker-block/picker-block.component.ts
@@ -3,12 +3,24 @@ import * as momentImported from 'moment';
 
 const moment = momentImported;
 
+export enum Stage {
+  next = 'next',
+  previous = 'previous'
+}
+
+export class Calendar {
+  week: number;
+  days: Array<momentImported.Moment>;
+}
+
 @Component({
   selector: 'ng2-picker-block',
   templateUrl: './picker-block.component.html',
   styleUrls: ['./picker-block.component.scss']
 })
 export class PickerBlockComponent implements OnInit {
+
+  st = Stage;
 
   dayNames: Array<string> = [
     'Su',
@@ -22,25 +34,66 @@ export class PickerBlockComponent implements OnInit {
 
   startWeek = moment().startOf('month').week();
   endWeek = moment().endOf('month').week();
+  pickerMonth: momentImported.Moment = moment();
 
-  currentMonth: string;
+  selectedDate: momentImported.Moment;
 
-  calendar = [];
+  calendar: Array<Calendar>;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.currentMonth = moment().format('MMMM');
+    this.selectedDate = moment();
 
+    this.setCalendar();
+  }
+
+  setCalendar(): void {
+    this.calendar = [];
     for (let week = this.startWeek; week <= this.endWeek; week++) {
+      console.log(week);
       this.calendar.push({
         week: week,
-        days: Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
+        days: Array(7).fill(0).map((n, i) => moment(this.pickerMonth).week(week).startOf('week').clone().add(n + i, 'day'))
       });
     }
-
-    console.log(this.calendar);
   }
+
+  changeMonth(stage: Stage) {
+    if (stage === this.st.previous) {
+      this.pickerMonth = moment(this.pickerMonth).subtract(1, 'month');
+      this.startWeek = this.pickerMonth.startOf('month').week();
+      this.endWeek = this.pickerMonth.endOf('month').week();
+      if (this.endWeek === 1) {
+        this.endWeek = 53;
+      }
+    }
+    if (stage === this.st.next) {
+      this.pickerMonth = moment(this.pickerMonth).add(1, 'month');
+      this.startWeek = this.pickerMonth.startOf('month').week();
+      this.endWeek = this.pickerMonth.endOf('month').week();
+      if (this.endWeek === 1) {
+        this.endWeek = 53;
+      }
+    }
+    this.setCalendar();
+  }
+
+  getDayClass(day: momentImported.Moment): string {
+    const dayClass: Array<string> = [];
+
+    if (day.format('MMMM') === this.pickerMonth.format('MMMM')) {
+      dayClass.push('current-month');
+    } else {
+      dayClass.push('not-current-month');
+    }
+
+    if (moment(day).startOf('day').isSame(this.selectedDate.startOf('day'))) {
+      dayClass.push('selected');
+    }
+    return dayClass.join(' ');
+  }
+
 
 }
